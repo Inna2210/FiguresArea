4ac5da2c2bf96ac56dec6341d37987f91c19dbb1
Inna2210 <inna.kurochkina@rambler.ru>
2022-04-11 20:51:12 +0300

    Version1

---
+++ Figures/FigureInformation/FigureInformation.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace FigureInformationLibrary
+{
+    public class FigureInformation
+    {
+        public double CalculateArea(Circle circle)
+        {
+            double area = Math.PI * circle.GetRadius() * circle.GetRadius();
+            return area; 
+        }
+
+        public double CalculateArea(Triangle triangle)
+        {
+            List<int> sides = triangle.GetSides();
+            double side1 = sides[0];
+            double side2 = sides[1];
+            double side3 = sides[2];
+            double semiPerimeter = ((side1 + side2 + side3) / 2);
+            double area = Math.Sqrt(semiPerimeter * (semiPerimeter - side1) * (semiPerimeter - side2) * (semiPerimeter - side3));
+
+            return area;
+        }
+    }
+
+    
+    public class Circle
+    {
+        public double radius;
+
+        public double GetRadius()
+        {
+            return radius;
+        }
+
+        public void SetRadius(double value)
+        {
+            radius = value;
+        }
+
+        public Circle(double radius)
+        {
+            this.SetRadius(radius);
+        }
+    }
+
+    public class Triangle
+    {
+        
+        List<int> sides = new();
+
+        public Triangle(List<int> sides)
+        {
+            this.SetSides(sides);
+        }
+
+        public List<int> GetSides()
+        {
+            return sides;
+        }
+
+        public void SetSides(List<int> value)
+        {
+            sides = value;
+        }
+
+        public bool CheckTriangleType(Triangle triangle)
+        {
+            List<int> sides = triangle.GetSides();
+            sides.Sort();
+            double side1 = sides[0];
+            double side2 = sides[1];
+            double side3 = sides[2];
+            bool type;
+            if (side3 * side3 == side1 * side1 + side2 * side2)
+            {
+                type = true;
+            }
+            else
+            {
+                type = false;
+            }
+            return type;
+        }
+    }
+}
---
+++ Figures/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using FigureInformationLibrary;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Figures
+{
+        public class Program
+        {
+            static void Main(string[] args)
+            {
+                do
+                {
+                    int optionFigureType = 0;
+                    do
+                    {
+                        Console.WriteLine("Вы хотите найти площадь треугольника или круга?");
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine(" 1 - площадь круга\t 2 - площадь треугольника");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        optionFigureType = Convert.ToInt32(Console.ReadLine());
+                        if (optionFigureType == 1 | optionFigureType == 2)
+                        {
+                            break;
+                        }
+                        Console.WriteLine("Сосредоточьтесь!");
+                    }
+                    while (optionFigureType != 1 | optionFigureType != 2);
+
+
+                    if (optionFigureType == 1)
+                    {
+                        Console.WriteLine("Введите, пожалуйста, радиус круга");
+                        FigureInformation figureInformation = new();
+                        Circle circle = new(double.Parse(Console.ReadLine()));
+                        double area = figureInformation.CalculateArea(circle);
+                        Console.WriteLine($"Площадь круга с заданным радиусом составляет {area}.");
+                    }
+
+                    if (optionFigureType == 2)
+                    {
+                        Console.WriteLine("Введите, пожалуйста, стороны треугольника через пробел или двоеточие");
+                        FigureInformation figureInformation = new();
+                        Triangle triangle = new(Console.ReadLine().Split(' ', ':', '\t').Select(i => Convert.ToInt32(i)).ToList());
+                        double area = figureInformation.CalculateArea(triangle);
+                        Console.WriteLine($"Площадь треугольника с заданными сторонами составляет {area}.");
+
+                        Console.WriteLine("Вы хотите проверить является ли треугольник прямоугольным?");
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine(" 1 - да\t 2 - нет");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        int optionTypeTriangle = Convert.ToInt32(Console.ReadLine());
+                        if (optionTypeTriangle == 1)
+                        {
+                            bool type = triangle.CheckTriangleType(triangle);
+                            if (type == true)
+                            {
+                                Console.WriteLine("Треугольник является прямоугольным");
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("Треугольник НЕ является прямоугольным");
+                            }
+                        }
+
+                    }
+
+                }
+                while (true);
+            }
+        }
+    
+}
